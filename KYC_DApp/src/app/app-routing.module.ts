0860a09516bfb233f1f8766ebe0534d21dea5c67
Ayushman <ayushmanraghav.ra@gmail.com>
2022-03-23 10:58:13 +0530

    add new Customer route

--- KYC_DApp/src/app/app-routing.module.ts
+++ KYC_DApp/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewCustomerComponent } from './new-customer/new-customer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "newCustomer", component: NewCustomerComponent},
+  {path: "**", redirectTo: "newCustomer", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
